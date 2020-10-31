87b8f307779c3dc986ca20db9128e8d196303dd7
Olver01 <arceolver01@gmail.com>
2020-10-31 13:08:36 -0500

    se pudo enviar el mapa al otro jugador, faltaria avisar si el misil le achunto a un barco

--- player.py
+++ player.py
@@ -10,6 +10,10 @@ class Player():
                 self.color=color
                 self.rect=(x,y,width,height)
                 self.vel=4
+                self.myship=pd.DataFrame(np.array([['-','-','-','-','-','-','-','-','-','-'],['-','-','-','-','-','-','-','-','-','-'],['-','-','-','-','-','-','-','-','-','-'],['-','-','-','-','-','-','-','-','-','-'],['-','-','-','-','-','-','-','-','-','-'],['-','-','-','-','-','-','-','-','-','-'],['-','-','-','-','-','-','-','-','-','-'],['-','-','-','-','-','-','-','-','-','-'],['-','-','-','-','-','-','-','-','-','-'],['-','-','-','-','-','-','-','-','-','-']]))
+                self.myatack=pd.DataFrame(np.array([['-','-','-','-','-','-','-','-','-','-'],['-','-','-','-','-','-','-','-','-','-'],['-','-','-','-','-','-','-','-','-','-'],['-','-','-','-','-','-','-','-','-','-'],['-','-','-','-','-','-','-','-','-','-'],['-','-','-','-','-','-','-','-','-','-'],['-','-','-','-','-','-','-','-','-','-'],['-','-','-','-','-','-','-','-','-','-'],['-','-','-','-','-','-','-','-','-','-'],['-','-','-','-','-','-','-','-','-','-']]))
+                self.posship=[]
+                self.posatack=[]
         def draw(self,win):
                 pygame.draw.rect(win,self.color,self.rect)
         def move(self):
@@ -25,5 +29,19 @@ class Player():
                 self.update()
         def update(self):
                 self.rect = (self.x,self.y,self.width,self.height)
-
-                
+        def getmyship(self):
+                return self.myship
+        def getmyatack(self):
+                return self.myatack
+        def setmyship(self,mapship):
+                self.myship=mapship
+        def setmyatack(self,mapatack):
+                self.myatack=mapatack
+        def getposship(self):
+                return self.posship
+        def getposatack(self):
+                return self.posatack
+        def setposship(self,posship):
+                self.posship=posship
+        def setposatack(self,posatack):
+                self.posatack=posatack
